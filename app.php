cc8d8bcbb7775893bb108352c7c5fe1478e9809e
onlyhash19 <hashhhspeed@gmail.com>
2025-01-31 11:14:44 -0600

    Repaso de php, flexbox, css

--- app.php
+++ app.php
@@ -1,6 +1,31 @@
 <h1>
-    PRUEBA php
+    Ola k ase?
 </h1>
 <?php
-    echo "Hola mundo"
+    $nombre = $_GET["nombre"];
+    if (!$nombre){
+        $nombre = "HASHLEY";
+    }
+
+    $texto = "<li>Repaso de PHP</li>";
+    if(isset($_GET["altura"])){
+        $altura = $_GET["altura"];
+    }else{
+        $altura = "No existe altura!";
+    }
+
+    $variable = "Hola amigo tu altura es:";
+    $textofinal = "<h2> ".$variable.$altura."cm.</h2>";
+
+    echo $texto; 
+    echo $textofinal;
+    echo "<hr>";
+    echo $_GET["nombre"];
+
+    if ($altura <= 150) {
+        echo '<h3 style="background:red; color:white;">Eres un umpalumpa xd</h3>'; 
+    }else{
+        echo '<h3 style="background:blue; color:white;">Eres una girafa</h3>'; 
+    }  
+
 ?>
